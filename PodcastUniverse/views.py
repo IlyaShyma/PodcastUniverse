4d001ab74e94da5614cdfa178e8a86eec0cac945
IlyaShyma <101025623+IlyaShyma@users.noreply.github.com>
2023-02-03 19:41:33 +0100

    a lot of stuff added

--- PodcastUniverse/views.py
+++ PodcastUniverse/views.py
@@ -1,16 +1,13 @@
 from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import DetailView
+from django.urls import reverse
+
 
 from profileManagementApp.models import UserProfile
-from podcastapp.models import Podcast
 from .forms import *
 from django.views.generic.edit import CreateView
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+
 
 
 def startpage(request):
@@ -31,22 +28,9 @@ def startpage(request):
         return render(request, template_name="homepage.html", context=context)
 
 
-
-
-# @login_required
-# def login_next_step(request):
-#     return render(request, template_name="registration_next_step.html")
-
-
-# def esempio(request):
-#     return render(request, template_name="fake_registration.html")
-
-
 class UserCreateView(CreateView):
     form_class = CreateRegularUser
     template_name = "user_create.html"
-    # success_url = reverse_lazy("registration2")
-    # success_url = reverse_lazy("profileManagementApp:user_settings")
 
     def form_valid(self, form_class):
         to_return = super().form_valid(form_class)
@@ -61,6 +45,7 @@ class UserCreateView(CreateView):
         # obj = form.instance or self.object
         user_profile_id = UserProfile.objects.get(user=self.object.pk).pk
         return reverse("profileManagementApp:user_settings", kwargs={'pk': user_profile_id})
+
 
 def user_logged_in(request):
     if request.user.is_authenticated == True:
